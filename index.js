19c4fc4ee01d128705b94d49547a96cbb5327d66
michepriest <miche.priest@gmail.com>
2018-08-31 20:08:19 +0000

    adding npm exercise

---
+++ index.js
@@ -0,0 +1,21 @@
+//var chalk = require("chalk");
+
+//var message = chalk.bgModel("Hello ") + chalk.blue("World");
+//console.log(message);
+var chalk = require("chalk");
+
+var message = chalk.blue("Hello ") + chalk.red("World");
+console.log(message);
+
+// var prettyjson = require('prettyjson');
+
+// var data = {
+//   username: 'michepriest'
+
+// };
+
+// console.log(prettyjson.render(data, {
+//   keysColor: 'rainbow',
+//   dashColor: 'magenta',
+//   stringColor: 'white'
+// }));
